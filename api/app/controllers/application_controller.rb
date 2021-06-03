fc136096e832b2fc11670e57d4e196c8df7dbcc4
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2021-06-03 17:57:21 -0300

    transform response in camelcase

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  private
+
+  def response_render_with(payload: {}, messages: [], status: :ok)
+    response_body = deep_transform_keys(payload: payload, messages: messages)
+
+    render json: response_body, status: status
+  end
+
+  def deep_transform_keys(data)
+    data.deep_transform_keys { |key| key.to_s.camelcase(:lower) }
+  end
 end
