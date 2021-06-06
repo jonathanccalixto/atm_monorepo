b2747cea0e02d18428ceae19ee72a31dfd969a47
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2021-06-06 00:37:56 -0300

    define sign in model

---
+++ web/src/models/SignInModel.js
@@ -0,0 +1,21 @@
+import Api from '../services/Api'
+import { ApiError } from '../services/Api/error'
+
+export const SignInModel = {
+  signIn: async data => {
+    try {
+      const response = await Api.post('/sign_in', { data })
+      const { payload, messages } = response.data
+
+      return [true, payload, messages]
+    } catch (error) {
+      if (error instanceof ApiError) {
+        const { payload, messages } = error.response.data
+
+        return [false, payload, messages]
+      }
+
+      throw error
+    }
+  },
+}
