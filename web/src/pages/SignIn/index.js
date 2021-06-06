cf153a8159c75b6091df4f36a146435e92595026
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2021-06-06 17:03:42 -0300

    implement sign in page

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,9 +1,78 @@
-import React from 'react'
+import React, { useCallback, useRef, useState } from 'react'
+import { useHistory } from 'react-router-dom'
 
-// import { Container } from './styles';
+import Input from '../../components/Input'
+import Messages from '../../components/Messages'
+
+import { SignInModel } from '../../models/SignInModel'
+import Api from '../../services/Api'
+
+import { Button, Container, Link } from './styles'
 
 const SignIn = () => {
-  return <div>Sign in</div>
+  const agencyRef = useRef()
+  const accountRef = useRef()
+  const passwordRef = useRef()
+
+  const history = useHistory()
+
+  const [successMessages, setSuccessMessages] = useState([])
+  const [errorMessages, setErrorMessages] = useState([])
+
+  const handleSubmit = useCallback(async event => {
+    event.preventDefault()
+
+    const signIn = {
+      account: accountRef.current?.getValue?.(),
+      agency: agencyRef.current?.getValue?.(),
+      password: passwordRef.current?.getValue?.(),
+    }
+
+    const [success, { token }, messages] = await SignInModel.signIn({ signIn })
+
+    if (success) {
+      Api.setAuthorization(token)
+
+      setSuccessMessages(messages)
+      setErrorMessages([])
+
+      history.push('/')
+    } else {
+      delete global.token
+      setSuccessMessages([])
+      setErrorMessages(messages)
+    }
+  }, [])
+
+  return (
+    <Container>
+      <Messages success={successMessages} error={errorMessages} />
+
+      <form onSubmit={handleSubmit}>
+        <Input ref={agencyRef} label="Agency:" id="agency" mask="9999" />
+        <Input
+          ref={accountRef}
+          label="Account:"
+          id="account"
+          mask="9999999-9"
+        />
+        <Input
+          ref={passwordRef}
+          label="Password:"
+          id="password"
+          type="password"
+        />
+
+        <Button type="submit" position={4} side="left">
+          Confirm
+        </Button>
+
+        <Link to="/sign_up" position={4} side="right">
+          Sign Up
+        </Link>
+      </form>
+    </Container>
+  )
 }
 
 export default SignIn
